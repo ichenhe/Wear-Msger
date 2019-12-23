ddea8920da78c17e74c6b4c27cbfea2e566c1fff
Chenhe <liangchenhe55@gmail.com>
2019-12-23 10:50:31 +0800

    chore: init

---
+++ wear/src/main/java/cc/chenhe/lib/wearmsger/MainActivity.kt
@@ -0,0 +1,15 @@
+package cc.chenhe.lib.wearmsger
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+
+class MainActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Enables Always-on
+        setAmbientEnabled()
+    }
+}
